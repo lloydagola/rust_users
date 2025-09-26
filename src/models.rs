6efaf351523bf965bbf822d3032afb7e5b372ae8
lloyd <lloydagola@gmail.com>
2025-09-26 15:14:58 +0300

    project set up

---
+++ src/models.rs
@@ -0,0 +1,16 @@
+
+user serde::{Serialize, Deserialize};
+use sqlx::FromRow;
+
+#[derive(Serialize, Deserialize, FromRow, Clone)]
+pub struct User{
+    pub id: String,
+    pub name: String,
+    pub age: u32,
+}
+
+#[derive(Deserialize)]
+pub struct UserPayload{
+    pub name: String,
+    pub age: u32,
+}

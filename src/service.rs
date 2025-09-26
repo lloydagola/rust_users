bad80ead1d5118a15b36f6b122733e08c2028ff6
lloyd <lloydagola@gmail.com>
2025-09-26 16:01:36 +0300

    consume Repo functions in services

---
+++ src/service.rs
@@ -0,0 +1,30 @@
+#[derrive(Clone)]
+pub struct UserService{
+    repo: UserRepository,
+}
+
+impl UserService{
+    pub fn new(repo: UserRepository) -> Self {
+        Self {repo}
+    }
+
+    pub async fn list_users(&self) -> Vec<User> {
+        self.repo.list().await
+    }
+
+    pub async fn create_user(&self, payload: UserPayload) -> User {
+        self.repo.create(payload).await
+    }
+
+    pub async fn get_user(&self, id: &str) -> Option<User> {
+        self.repo.get(id).await
+    }
+
+    pub async update_user(&self, id: &str, payload: UserPayload) -> Option<User> {
+        self.repo.update(id, payload).await
+    }
+
+    pub async fn delete_user(&self, id: &str) -> bool {
+        self.repo.delete(id).await
+    }
+}
